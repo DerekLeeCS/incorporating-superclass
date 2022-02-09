0332e1c6d183b4582b7d1855030e23f10257e0f6
Derek <51767748+DerekLeeCS@users.noreply.github.com>
2022-02-09 15:07:46 -0500

    Test new version of SCINet

--- src/models/scinet.py
+++ src/models/scinet.py
@@ -16,6 +16,10 @@ class SCIN(tf.keras.layers.Layer):
         self.beta = tf.Variable(tf.random.normal(shape=(num_superclasses, num_channels), mean=0, stddev=0.5),
                                 dtype=tf.float32, trainable=True)
 
+    def get_config(self):
+        config = super().get_config()
+        return config
+
     def call(self, x: tf.Tensor, super_ind: tf.Tensor, **kwargs) -> tf.Tensor:
         sig = tf.nn.embedding_lookup(self.gamma, super_ind)
         mu = tf.nn.embedding_lookup(self.beta, super_ind)
@@ -41,43 +45,20 @@ class SCIN(tf.keras.layers.Layer):
         return x
 
 
-def residual_block_with_scin(inputs: Tuple[tf.Tensor, int], num_superclasses: int, filters: Tuple[int, int], s: int = 1,
-                             conv_shortcut: bool = False) -> tf.Tensor:
+def residual_block_with_scin(inputs: Tuple[tf.Tensor, int], num_superclasses: int, num_channels: int) -> tf.Tensor:
     """
     :param inputs: a tuple containing the input tensor and the sparse superclass prediction
     :param num_superclasses: the number of superclasses in the dataset
-    :param filters: a tuple containing the two filters for the residual block
-    :param s: the stride to downsample with
-    :param conv_shortcut: whether to use a convolutional shortcut or the identity shortcut
+    :param num_channels: the number of channels in the input
     :return: the output of the residual block
     """
     input_tensor, super_ind = inputs
-    f1, f2 = filters
-    k = 3  # Kernel size
 
     pre_act = tf.keras.layers.BatchNormalization()(input_tensor)
     pre_act = tf.keras.layers.ReLU()(pre_act)
 
-    if conv_shortcut:
-        x_short = tf.keras.layers.Conv2D(f2, kernel_size=(1, 1), strides=(s, s),
-                                         kernel_regularizer=REGULARIZER)(pre_act)
-    else:
-        x_short = tf.keras.layers.MaxPooling2D((1, 1), strides=(s, s))(input_tensor) if s > 1 else input_tensor
-
-    # Block 1
-    x = tf.keras.layers.Conv2D(f1, kernel_size=(1, 1), strides=(1, 1), kernel_regularizer=REGULARIZER)(pre_act)
-
-    # Block 2
-    x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.keras.layers.ReLU()(x)
-    x = tf.keras.layers.Conv2D(f1, kernel_size=(k, k), strides=(s, s), padding='same',
-                               kernel_regularizer=REGULARIZER)(x)
-    x = SCIN(num_superclasses, f1)(x, super_ind)
-
-    # Block 3
-    x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.keras.layers.ReLU()(x)
-    x = tf.keras.layers.Conv2D(f2, kernel_size=(1, 1), strides=(1, 1), kernel_regularizer=REGULARIZER)(x)
+    x_short = input_tensor
+    x = SCIN(num_superclasses, num_channels)(pre_act, super_ind)
 
     # Output
     x += x_short
@@ -114,7 +95,7 @@ class SCINet(BaseModule):
         # x = SCIN(num_superclasses, 1024)(x, super_ind)
 
         # Stage 4
-        x = residual_block_with_scin((x, super_ind), num_superclasses=num_superclasses, filters=(512, 2048), s=2)
+        x = residual_block_with_scin((x, super_ind), num_superclasses=num_superclasses, num_channels=1024)
         # x = stack_blocks(x, filters=(512, 2048), num_blocks=3)
 
         # Pooling
